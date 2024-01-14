88f345066f6e2579df27c2df84bd8f23396636f9
Kenta <m.kenta1105@outlook.jp>
2024-01-14 18:40:13 +0900

    ■インターンシップ用コード追加

---
+++ src/processPointClouds.cpp
@@ -0,0 +1,170 @@
+// PCL lib Functions for processing point clouds 
+
+#include "processPointClouds.h"
+
+
+//constructor:
+template<typename PointT>
+ProcessPointClouds<PointT>::ProcessPointClouds() {}
+
+
+//de-constructor:
+template<typename PointT>
+ProcessPointClouds<PointT>::~ProcessPointClouds() {}
+
+
+template<typename PointT>
+void ProcessPointClouds<PointT>::numPoints(typename pcl::PointCloud<PointT>::Ptr cloud)
+{
+    std::cout << cloud->points.size() << std::endl;
+}
+
+
+template<typename PointT>
+typename pcl::PointCloud<PointT>::Ptr ProcessPointClouds<PointT>::FilterCloud(typename pcl::PointCloud<PointT>::Ptr cloud, float filterRes, Eigen::Vector4f minPoint, Eigen::Vector4f maxPoint)
+{
+
+    // Time segmentation process
+    auto startTime = std::chrono::steady_clock::now();
+
+    // TODO:: Fill in the function to do voxel grid point reduction and region based filtering
+
+    auto endTime = std::chrono::steady_clock::now();
+    auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    std::cout << "filtering took " << elapsedTime.count() << " milliseconds" << std::endl;
+
+    return cloud;
+
+}
+
+
+template<typename PointT>
+std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> ProcessPointClouds<PointT>::SeparateClouds(pcl::PointIndices::Ptr inliers, typename pcl::PointCloud<PointT>::Ptr cloud) 
+{
+  // TODO: Create two new point clouds, one cloud with obstacles and other with segmented plane
+
+    std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> segResult(cloud, cloud);
+    return segResult;
+}
+
+
+template<typename PointT>
+std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> ProcessPointClouds<PointT>::SegmentPlane(typename pcl::PointCloud<PointT>::Ptr cloud, int maxIterations, float distanceThreshold)
+{
+    // Time segmentation process
+    auto startTime = std::chrono::steady_clock::now();
+	pcl::PointIndices::Ptr inliers;
+    // TODO:: Fill in this function to find inliers for the cloud.
+
+    auto endTime = std::chrono::steady_clock::now();
+    auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    std::cout << "plane segmentation took " << elapsedTime.count() << " milliseconds" << std::endl;
+
+    std::pair<typename pcl::PointCloud<PointT>::Ptr, typename pcl::PointCloud<PointT>::Ptr> segResult = SeparateClouds(inliers,cloud);
+    return segResult;
+}
+
+
+template<typename PointT>
+std::vector<typename pcl::PointCloud<PointT>::Ptr> ProcessPointClouds<PointT>::Clustering(typename pcl::PointCloud<PointT>::Ptr cloud, float clusterTolerance, int minSize, int maxSize)
+{
+
+    // Time clustering process
+    auto startTime = std::chrono::steady_clock::now();
+
+    std::vector<typename pcl::PointCloud<PointT>::Ptr> clusters; // クラスターの配列
+
+    // TODO:: 関数を実装して下さい
+    // 1. KD-Treeの設定
+    pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>); // 木の設定
+    tree->setInputCloud (cloud);                                             // 点群の設定
+
+    // 2. ユークリッドクラスタリングの実行
+    std::vector<pcl::PointIndices> cluster_indices;                          // クラスターのインデックス
+    pcl::EuclideanClusterExtraction<PointT> ec;                              // ユークリディアンクラスターオブジェクト
+    ec.setClusterTolerance (clusterTolerance);                               // 許容値
+    ec.setMinClusterSize (minSize);                                          // クラスターの最小点群個数の設定
+    ec.setMaxClusterSize (maxSize);                                          // クラスターの最大点群個数の設定
+    ec.setSearchMethod (tree);                                               // 探索方法にKD-Treeを設定
+    ec.setInputCloud (cloud_filtered);                                       // 点群を設定
+    ec.extract (cluster_indices);                                            // クラスターのインデックスを取得
+
+    // 3. クラスターごとに分類
+    for (pcl::PointIndices getIndices : cluster_indices)
+    {
+        pcl::PointCloud<PointT>::Ptr cloud_cluster (new pcl::PointCloud<PointT>); 
+        for (int index : getIndices.indices)
+        {
+            cloud_cluster.points.push_back(cloud->points[index]);
+        }
+        cloud_cluster->width    = cloud_cluster->size ();
+        cloud_cluster->height   = 1;
+        cloud_cluster->is_dense = true;
+
+        clusters.push_back(cloud_cluster);
+    }
+
+    auto endTime = std::chrono::steady_clock::now();
+    auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    std::cout << "clustering took " << elapsedTime.count() << " milliseconds and found " << clusters.size() << " clusters" << std::endl;
+
+    return clusters;
+}
+
+
+template<typename PointT>
+Box ProcessPointClouds<PointT>::BoundingBox(typename pcl::PointCloud<PointT>::Ptr cluster)
+{
+
+    // Find bounding box for one of the clusters
+    PointT minPoint, maxPoint;
+    pcl::getMinMax3D(*cluster, minPoint, maxPoint);
+
+    Box box;
+    box.x_min = minPoint.x;
+    box.y_min = minPoint.y;
+    box.z_min = minPoint.z;
+    box.x_max = maxPoint.x;
+    box.y_max = maxPoint.y;
+    box.z_max = maxPoint.z;
+
+    return box;
+}
+
+
+template<typename PointT>
+void ProcessPointClouds<PointT>::savePcd(typename pcl::PointCloud<PointT>::Ptr cloud, std::string file)
+{
+    pcl::io::savePCDFileASCII (file, *cloud);
+    std::cerr << "Saved " << cloud->points.size () << " data points to "+file << std::endl;
+}
+
+
+template<typename PointT>
+typename pcl::PointCloud<PointT>::Ptr ProcessPointClouds<PointT>::loadPcd(std::string file)
+{
+
+    typename pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>);
+
+    if (pcl::io::loadPCDFile<PointT> (file, *cloud) == -1) //* load the file
+    {
+        PCL_ERROR ("Couldn't read file \n");
+    }
+    std::cerr << "Loaded " << cloud->points.size () << " data points from "+file << std::endl;
+
+    return cloud;
+}
+
+
+template<typename PointT>
+std::vector<boost::filesystem::path> ProcessPointClouds<PointT>::streamPcd(std::string dataPath)
+{
+
+    std::vector<boost::filesystem::path> paths(boost::filesystem::directory_iterator{dataPath}, boost::filesystem::directory_iterator{});
+
+    // sort files in accending order so playback is chronological
+    sort(paths.begin(), paths.end());
+
+    return paths;
+
+}
---
+++ src/quiz/cluster/kdtree.h
@@ -0,0 +1,123 @@
+/* \author Aaron Brown */
+// Quiz on implementing kd tree
+
+#include "../../render/render.h"
+
+
+// Structure to represent node of kd tree
+struct Node
+{
+	std::vector<float> point;
+	int id;
+	Node* left;
+	Node* right;
+
+	Node(std::vector<float> arr, int setId)
+	:	point(arr), id(setId), left(NULL), right(NULL)
+	{}
+
+	~Node()
+	{
+		delete left;
+		delete right;
+	}
+};
+
+struct KdTree
+{
+	Node* root;
+
+	KdTree()
+	: root(NULL)
+	{}
+
+	~KdTree()
+	{
+		delete root;
+	}
+
+	void insertHelper(Node** node, int depth, std::vector<float> point, int id)
+	{
+		if (*node == NULL) // ノードが空の時
+		{
+			*node = new Node(point, id);
+		}
+		else
+		{
+			int axis = depth%2; // 分割の軸
+			//int axis = point.size(); // 何次元でも対応可
+			if (point[axis] < ((*node)->point[axis]))
+			{
+				insertHelper(&((*node)->left), depth+1, point, id);
+			}
+			else
+			{
+				insertHelper(&((*node)->right), depth+1, point, id);
+			}
+		}
+	}
+
+	void insert(std::vector<float> point, int id)
+	{
+		// TODO: Fill in this function to insert a new point into the tree
+		insertHelper(&root, 0, point, id)
+	}
+
+	void searchHelper(Node* node, int depth, std::vector<float> target, float distanceTol, std::vector<int>& ids)
+	{
+		if (node != NULL)
+		{
+			// 四角形に入ってるかどうかのチェック
+			bool inBox = true;
+			inBox = (node->point[0] >= (target[0]-distanceTol) && node->point[0] <= (target[0]+distanceTol) && node->point[1] >= (target[1]-distanceTol) && node->point[1] <= (target[1]+distanceTol))
+			// // 何次元でも対応可能
+			// for (int i=0; i<target.size(); i++)
+			// {
+			// 	inBox &= (std::abs(node->point[i] - target[i]) <= distanceTol); 
+			// }
+			// 円内に入ってるかのチェック and クラスターにidの追加
+			if (inCheckBox)
+			{
+				float dist = std::sqrt((node->point[0]-target[0])*(node->point[0]-target[0]) + (node->point[1]-target[1])*(node->point[1]-target[1]));
+				if (dist <= distanceTol)
+				{
+					ids.push_back(node->id);
+				}
+				// // 拡張ver
+				// float dist = 0.0;
+				// for (int i=0; i<target.size(); i++)
+				// {
+				// 	dist += ((node->point[i] - target[i]) * (node->point[i] - target[i]));
+				// }
+				// if (dist <= (distanceTol*distanceTol))
+				// {
+				// 	ids.push_back(node->id);
+				// }
+			}
+		}
+		// 次のノードの探索
+		int axis = depth % 2;
+		if ((target[axis]-distanceTol) < node->point[axis])
+		{
+			searchHelper(node->left, depth+1, target, distanceTol, ids);
+		}
+		if ((target[axis]-distanceTol) > node->point[axis])
+		{
+			searchHelper(node->left, depth+1, target, distanceTol, ids);
+		}
+
+	}
+	// return a list of point ids in the tree that are within distance of target
+	std::vector<int> search(std::vector<float> target, float distanceTol)
+	{
+		std::vector<int> ids;
+		searchHelper(root, 0, target, distanceTol, ids);
+		return ids;
+	}
+	
+
+};
+
+
+
+
